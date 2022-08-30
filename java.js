7dad0e205da748eb2ee44e1d6ef5cc02c2efb9dd
reerajput930 <rajputriya930@gmail.com>
2022-08-30 09:55:56 +0530

    from onclick to addEventListener

--- java.js
+++ java.js
@@ -1,28 +1,28 @@
-let screen = document.getElementById("screen")
-let user_input= document.querySelectorAll("button")
-let screen_input='';
+let screen = document.getElementById("screen");
+let user_input = document.querySelectorAll("button");
+let screen_input = "";
+
+
 for(items of user_input){
-    items.onclick= function(e){
-        let button_text = e.target.innerHTML;
-        console.log(button_text);
+items.addEventListener("click",(e)=>{
+  console.log("helo");
+  let button_text = e.target.innerHTML;
+  console.log(button_text);
 
-        if(button_text == 'x'){
-             button_text = '*'
-            screen_input +=button_text;
-            screen.value = screen_input;
-        }
-        else if(button_text=='C'){
-            screen_input="";
-            screen.value = "";
-        }
-        else if(button_text== '='){
-            screen.value = eval(screen_input)
-            screen_input ="";
-        }
-
-        else{
-            screen_input += button_text;
-            screen.value = screen_input;
-        }
-    }
+  if (button_text == "x") {
+    button_text = "*";
+    screen_input += button_text;
+    screen.value = screen_input;
+  } else if (button_text == "C") {
+    screen_input = "";
+    screen.value = "";
+  } else if (button_text == "=") {
+    // eval function eval the string
+    screen.value = eval(screen_input);
+    screen_input = "";
+  } else {
+    screen_input += button_text;
+    screen.value = screen_input;
+  }
+});
 }
